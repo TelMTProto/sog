f85d72e0b2e2a0c345f48851bd51ad2bbf3eaa3c
TelMTProto <117292475+TelMTProto@users.noreply.github.com>
2023-07-04 06:52:22 -0700

    Update config.py

--- config.py
+++ config.py
@@ -20,10 +20,10 @@ MODES = {
 
 # The domain for TLS mode, bad clients are proxied there
 # Use random existing domain, proxy checks it on start
-TLS_DOMAIN = "www.irancell.io"
+TLS_DOMAIN = "www.aliexpress.com"
 
 # Tag for advertising, obtainable from @MTProxybot
-AD_TAG = "c9f96504e1e6ffe934f134baf7ca5255"
+AD_TAG = "4689c8539e952ba0cf3df80b0e030e48"
 
 #7gAAAAAAAAAAAAAAAAAAAAB3d3cuaXJhbmNlbGwuaW8
 #curl -o MTProo.sh -L https://git.io/fjo3u && bash MTProo.sh --port 443 --secret 00000000000000000000000000000000 --tag 8bd7c59cd625d7991b86a77dfbc5fcff --tls www.HideProxi.io --disable-updater
